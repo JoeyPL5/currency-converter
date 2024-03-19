8a6bbf504e9b1ade1d97bb7b9d57f22f7b3e3f4f
unknown <pierre-louis.jo@northeastern.edu>
2024-03-19 16:36:11 -0400

    basic setup, logo, header, selectable currency, connects to backend

--- frontend/currency-converter/src/App.js
+++ frontend/currency-converter/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import currencyconversionlogo from './images/currencyconversionlogo.png';
+import SelectableCurrency from './SelectableCurrency'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p color="black">
+          Joseph Pierre-Louis's Currency Converter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <img id="logo" src={currencyconversionlogo} className="App-logo" alt="logo" />
       </header>
+      <SelectableCurrency/>
     </div>
   );
 }
